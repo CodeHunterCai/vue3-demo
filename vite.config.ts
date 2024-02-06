3c6705d7ccb6951dc4870b784988392836fd6bd6
yunpeng.cai <yunpeng.cai@opay-inc.com>
2024-02-06 14:42:34 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,41 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  console.log(env)
+
+  return {
+    server: {
+      host: '0.0.0.0',
+      open: true,
+      cors: true,
+      proxy: {
+        '/api/safety/v1': {
+          target: env.VITE_PROXY_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            title: env.VITE_GLOB_APP_TITLE
+          }
+        }
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
